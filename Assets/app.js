803401a05b02cdae9185be669788fcd59d10f6e5
MikeFogz <MikeFogz@gmail.com>
2020-11-07 22:05:51 -0800

    UPDATE: Current Weather API data now pushing to page.

--- Assets/app.js
+++ Assets/app.js
@@ -1,13 +1,13 @@
 // Declare variables
 // var cityName = document.querySelector("#city-name")
-var cityName = "Long Beach"
-var apiKey = "d6023dc0b7605243dca140129a0bd39b"
+var cityName = "Oakland"
+//var apiKey = "d6023dc0b7605243dca140129a0bd39b"
 
 // current weather api url
-// var requestWeatherUrl= 'api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey ;
-var requestWeatherUrl= 'https://api.openweathermap.org/data/2.5/weather?q=Long Beach&units=imperial&appid=d6023dc0b7605243dca140129a0bd39b'
+var requestWeatherUrl= 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=d6023dc0b7605243dca140129a0bd39b' ;
+//var requestWeatherUrl= 'https://api.openweathermap.org/data/2.5/weather?q=Long Beach&units=imperial&appid=d6023dc0b7605243dca140129a0bd39b'
 //5 day forecast api url
-var requestForecastUrl= 'api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=' + apiKey ;
+//var requestForecastUrl= 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=' + apiKey ;
 
 // // event listener
 // document.getElementById('getCity').addEventListener('submit', getCity);
@@ -32,19 +32,30 @@ function getApi(requestWeatherUrl) {
     })
     .then(function (data) {
         console.log(data);
-    })
         //City + Date + Icon
 
+        //weather[0].id
+
         //Temp
-        var temp = data.list[0].main.temp;
+        var temp = data.main.temp;
         tempP = document.createElement('p');
         var tempEl = document.querySelector("#weather-div")
         tempEl.append(tempP);
         tempP.textContent = ("Current Temperature: " + temp + "\u00B0F");
         //Humidity
-
-        //WInd Speed
-
+        var humid = data.main.humidity;
+        humidP = document.createElement('p');
+        var humidEl = document.querySelector("#weather-div")
+        humidEl.append(humidP);
+        humidP.textContent = ("Humidity: " + humid);
+        //Wind Speed
+        var wind = data.wind.speed;
+        windP = document.createElement('p');
+        var windEl = document.querySelector("#weather-div")
+        windEl.append(windP);
+        windP.textContent = ("Wind Speed: " + wind + " MPH");
         // UV Index
+
+    })
 };
 getApi(requestWeatherUrl);
